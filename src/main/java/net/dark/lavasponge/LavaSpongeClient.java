2d8de731f4cd17ad46a10f66c069abdb65cb9db7
Dark <tomnic18@gmail.com>
2025-05-11 15:47:23 +0200

    2nd commit

---
+++ src/main/java/net/dark/lavasponge/LavaSpongeClient.java
@@ -0,0 +1,10 @@
+package net.dark.lavasponge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LavaSpongeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
